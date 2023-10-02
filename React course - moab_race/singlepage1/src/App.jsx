f2205645a549d6261a5878264fdb84423946a1cf
Claire <eaaclhb@students.eaaa.dk>
2023-10-02 12:54:50 +0200

    Greeting / props

--- React course - moab_race/singlepage1/src/App.jsx
+++ React course - moab_race/singlepage1/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Columns from "./components/Columns";
 import Footer from "./components/Footer";
+import Greeting from "./components/Greeting";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
@@ -8,6 +9,9 @@ function App() {
   return (
     <>
       <Home />
+      <Greeting name="Natalia" mystyle="red" />
+      <Greeting name="Rasmus" mystyle="green" />
+      <Greeting name="Claire" mystyle="blue" />
       <Header />
       <Columns />
       <Footer />
---
+++ React course - moab_race/singlepage1/src/components/Greeting.jsx
@@ -0,0 +1,20 @@
+export default function Greeting(props) {
+  const hi = "Hi, how are you ";
+
+  return (
+    <p style={{ color: props.mystyle }}>
+      {hi}
+      {props.name}?
+    </p>
+  );
+}
+
+/*export default function Greeting({ name, mystyle }) {
+  const hi = "Hi, how are you ";
+
+  return (
+    <p style={{ color: mystyle }}>
+      {hi} {name}?
+    </p>
+  );
+}*/
